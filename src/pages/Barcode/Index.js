4df711e74a6166663d4b27968b089cbba7329b5d
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2025-04-07 12:43:59 +0530

    manage  payamnet:

--- src/pages/Barcode/Index.js
+++ src/pages/Barcode/Index.js
@@ -4,10 +4,16 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { Scanner } from "@yudiel/react-qr-scanner";
 import toast from "react-hot-toast";
+import Popup from "@/components/Popup";
+import { BsQrCodeScan } from "react-icons/bs";
 
 export default function QRScanner() {
-  const [data, setData] = useState("");
   const router = useRouter();
+  const [data, setData] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
 
   const handleScan = (result) => {
     console.log("result", result)
@@ -27,9 +33,29 @@ export default function QRScanner() {
 
   return (
     <div>
-      <p>Scanned Data: {data}</p>
-      <h1>QR Code Scanner</h1>
-      <Scanner onScan={handleScan} />
+      <button
+        onClick={toggleModal}
+        className='font-inter font-medium lg:text-[16px] text-[14px] gap-2 leading-tight items-center  text-center xxl:w-52 px-4 border-2 py-3  mb-2 rounded-lg lg:rounded-xl focus:outline-none  flex  '
+      >
+        <BsQrCodeScan size={20} className='text-black-600 hover:text-black-700' /> Scan
+      </button>
+
+      {isOpen && (
+        <Popup isOpen={isOpen} onClose={toggleModal} size={"max-w-[400px]"} >
+          <div className="p-4 space-y-4">
+            <h2 className="text-xl font-semibold text-gray-800">
+              Scan Qrcode
+            </h2>
+            <div className="scanner-wrapper">
+              <p>Scanned Data: {data}</p>
+              <h1>QR Code Scanner</h1>
+              <Scanner onScan={handleScan} />
+
+            </div>
+          </div>
+        </Popup>
+      )}
+
     </div>
   );
 }
